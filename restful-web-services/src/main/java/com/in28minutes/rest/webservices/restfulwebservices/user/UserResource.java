36e7179cc7b4a6e0d8a259f563e09cb919fc60a5
Xabier <xabier.martinez@taiger.com>
2019-04-24 23:26:27 +0200

    Video 31

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,14 +1,8 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.Resource;
-import org.springframework.hateoas.mvc.ControllerLinkBuilder;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.validation.Valid;
-import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,45 +19,14 @@ public class UserResource {
 
     // retrieveUser(int id)
     @GetMapping("/users/{id}")
-    public Resource<User> retrieveUser(@PathVariable int id) {
-        User user = service.findOne(id);
-
-        if (user == null) {
-            throw new UserNotFoundException("id-" + id);
-        }
-
-        // HATEOAS
-        Resource<User> resource = new Resource<User>(user);
-
-        ControllerLinkBuilder linkTo =
-                ControllerLinkBuilder.linkTo(ControllerLinkBuilder.methodOn(this.getClass()).retrieveAllUsers());
-
-        resource.add(linkTo.withRel("all-users"));
-
-        return resource;
-    }
-
-    // deleteUser(int id)
-    @DeleteMapping("/users/{id}")
-    public void deleteUser(@PathVariable int id) {
-        User user = service.deleteById(id);
-
-        if (user == null) {
-            throw new UserNotFoundException("id-" + id);
-        }
+    public User retrieveUser(@PathVariable int id) {
+        return service.findOne(id);
     }
 
     // input -details of user
     // output - CREATED & Return the created URI
     @PostMapping("/users")
-    public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
+    public void createUser(@RequestBody User user) {
         User savedUser = service.save(user);
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(savedUser.getId())
-                .toUri();
-
-        return ResponseEntity.created(location).build();
     }
 }
